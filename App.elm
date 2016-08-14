655e9a849dbdcf08a2b68dcb51cabbcfee899c15
paul wisehart <paul@oldcode.org>
2016-08-14 00:14:01 -0400

    git Widget to work with a Widget2, next get 2 instances of Widget ... elm record update syntax is weird.

--- App.elm
+++ App.elm
@@ -3,24 +3,29 @@ module Main exposing (..)
 import Html exposing (Html)
 import Html.App
 import Widget
+import Widget2
 
 -- MODEL
-type alias AppModel = { widgetModel : Widget.Model }
+type alias AppModel = { widgetModel : Widget.Model 
+                      , widgetModel2 : Widget2.Model }
 
 initialModel : AppModel
-initialModel = { widgetModel = Widget.initialModel }
+initialModel = { widgetModel = Widget.initialModel 
+               , widgetModel2 = Widget2.initialModel }
 
 init : ( AppModel, Cmd Msg )
 init = ( initialModel, Cmd.none )
 
 -- MESSAGES
-type Msg = WidgetMsg Widget.Msg
+type Msg = WidgetMsg Widget.Msg | WidgetMsg2 Widget2.Msg
+-- type Msg = WidgetMsg2 Widget2.Msg
 
 -- VIEW
 view : AppModel -> Html Msg
 view model =
     Html.div []
         [ Html.App.map WidgetMsg (Widget.view model.widgetModel)
+        , Html.App.map WidgetMsg2 (Widget2.view model.widgetModel2)
         ]
 
 -- UPDATE
@@ -33,6 +38,13 @@ update message model =
                     Widget.update subMsg model.widgetModel
             in
                 ( { model | widgetModel = updatedWidgetModel }, Cmd.map WidgetMsg widgetCmd )
+
+        WidgetMsg2 subMsg2 ->
+            let
+                ( updatedWidgetModel, widgetCmd ) =
+                    Widget2.update subMsg2 model.widgetModel2
+            in
+                ( { model | widgetModel2 = updatedWidgetModel }, Cmd.map WidgetMsg2 widgetCmd )
 
 -- SUBSCIPTIONS
 subscriptions : AppModel -> Sub Msg
